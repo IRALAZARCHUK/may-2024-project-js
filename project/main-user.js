c152705859ea74c2e71a69f85722f5179fa6d1f0
IRALAZARCHUK <ira.lazarchuk12@gmail.com>
2024-09-04 21:48:57 +0300

    project-js

--- project/main-user.js
+++ project/main-user.js
@@ -3,62 +3,58 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(users => {
         const container = document.getElementById('full-info');
 
-
         let currentUserId = null;
 
         for (const user of users) {
             const div = document.createElement('div');
             div.classList.add('newDiv');
             const usersInfo = document.createElement('div');
-            usersInfo.innerText = `
-                id: ${user.id},
-                name: ${user.name},
-                username: ${user.username},
-                email: ${user.email},
-                address: 
-                  street: ${user.address.street},
-                  suite: ${user.address.suite},
-                  city: ${user.address.city},
+            usersInfo.innerHTML = `
+                <p>id: ${user.id}</p>
+                <p>name: ${user.name}</p>
+                <p>username: ${user.username}</p>
+                <p>email: ${user.email}</p>
+                <p>address: 
+                  street: ${user.address.street}, 
+                  suite: ${user.address.suite}, 
+                  city: ${user.address.city}, 
                   zipcode: ${user.address.zipcode},
-                  geo:
-                    lat: ${user.address.geo.lat},
-                    lng: ${user.address.geo.lng},
-                phone: ${user.phone},
-                website: ${user.website},
-                company: 
-                  name: ${user.company.name},
-                  catchPhrase: ${user.company.catchPhrase},
+                  geo: lat: ${user.address.geo.lat}, lng: ${user.address.geo.lng}
+                </p>
+                <p>phone: ${user.phone}</p>
+                <p>website: ${user.website}</p>
+                <p>company: 
+                  name: ${user.company.name}, 
+                  catchPhrase: ${user.company.catchPhrase}, 
                   bs: ${user.company.bs}
+                </p>
             `;
+            const buttonPost = document.createElement('button');
+            buttonPost.innerText = 'Post of current user';
+            buttonPost.onclick = () => {
+                showUserPosts(user.id);
+            };
+
             div.appendChild(usersInfo);
-
-            const button = document.createElement('button');
-            button.innerText = 'Show Posts';
-            button.addEventListener('click', () => {
-                currentUserId = user.id;
-                fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
-                    .then(response => response.json())
-                    .then(posts => {
-                        const postsBox = document.getElementById('posts');
-                        postsBox.innerHTML = '';
-
-                        posts.forEach(post => {
-                            const postDiv = document.createElement('div');
-                            postDiv.classList.add('post');
-                            postDiv.innerHTML = `
-                                <h2>${post.title}</h2>  
-                                <button data-post-id="${post.id}">
-                                    Post Details
-                                </button>
-                            `;
-                            postsBox.appendChild(postDiv);
-                        });
-                    })
-
-            });
-
-            div.appendChild(button);
+            div.appendChild(buttonPost);
             container.appendChild(div);
         }
-
     })
+
+function showUserPosts(userId) {
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+        .then(response => response.json())
+        .then(posts => {
+            const postsContainer = document.getElementById('posts-container') || document.createElement('div');
+            postsContainer.id = 'posts-container';
+            postsContainer.innerHTML = '';
+            posts.forEach(post => {
+                const postElement = document.createElement('p');
+                postElement.innerText = `Title: ${post.title}`;
+                postsContainer.appendChild(postElement);
+            });
+
+            const userDiv = document.querySelector(`#full-info .newDiv:nth-child(${userId})`);
+            userDiv.appendChild(postsContainer);
+        });
+}
