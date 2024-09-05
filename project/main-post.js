510a4e8005bac416aba5a8f2e23ebda305a9ee2d
IRALAZARCHUK <ira.lazarchuk12@gmail.com>
2024-09-05 20:56:39 +0300

    project-js

---
+++ project/main-post.js
@@ -0,0 +1,72 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => {
+        const container = document.getElementById('post-info');
+
+        for (const post of posts) {
+            const div = document.createElement('div');
+            div.classList.add('newDiv');
+
+            const usersInfo = document.createElement('div');
+            usersInfo.innerHTML = `
+                <h2>Title: ${post.title}</h2>
+                <p>Body: ${post.body}</p>
+                <p><strong>User ID:</strong> ${post.userId}</p>
+                <p><strong>Post ID:</strong> ${post.id}</p>
+            `;
+
+            const buttonPost = document.createElement('button');
+            buttonPost.innerText = 'Show Comments';
+            buttonPost.setAttribute('data-visible', 'false');
+
+            buttonPost.onclick = () => {
+                const isVisible = buttonPost.getAttribute('data-visible') === 'true';
+
+                if (isVisible) {
+                    hideUserComments(post.id);
+                    buttonPost.innerText = 'Show Comments';
+                    buttonPost.setAttribute('data-visible', 'false');
+                } else {
+                    showUserComments(post.id);
+                    buttonPost.innerText = 'Hide Comments';
+                    buttonPost.setAttribute('data-visible', 'true');
+                }
+            };
+
+            div.appendChild(usersInfo);
+            div.appendChild(buttonPost);
+            container.appendChild(div);
+        }
+    });
+
+
+function showUserComments(postId) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+        .then(response => response.json())
+        .then(comments => {
+            const commentContainer = document.getElementById(`comments-container-${postId}`) || document.createElement('div');
+            commentContainer.id = `comments-container-${postId}`;
+            commentContainer.innerHTML = '';
+
+            comments.forEach(comment => {
+                const commentElement = document.createElement('div');
+                commentElement.innerHTML = `
+                    <p><strong>${comment.name}</strong> (${comment.email})</p>
+                    <p>${comment.body}</p>
+                    <hr/>
+                `;
+                commentContainer.appendChild(commentElement);
+            });
+
+            const postDiv = document.querySelector(`#post-info .newDiv:nth-child(${postId})`);
+            postDiv.appendChild(commentContainer);
+        });
+}
+
+
+function hideUserComments(postId) {
+    const commentContainer = document.getElementById(`comments-container-${postId}`);
+    if (commentContainer) {
+        commentContainer.remove();
+    }
+}
--- project/main-user.js
+++ project/main-user.js
@@ -1,9 +1,8 @@
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => {
         const container = document.getElementById('full-info');
-
-        let currentUserId = null;
 
         for (const user of users) {
             const div = document.createElement('div');
@@ -31,30 +30,53 @@ fetch('https://jsonplaceholder.typicode.com/users')
             `;
             const buttonPost = document.createElement('button');
             buttonPost.innerText = 'Post of current user';
+            buttonPost.setAttribute('data-visible', 'false');
+
             buttonPost.onclick = () => {
-                showUserPosts(user.id);
+                const isVisible = buttonPost.getAttribute('data-visible') === 'true';
+
+                if (isVisible) {
+                    hideUserPosts(user.id);
+                    buttonPost.innerText = 'Post of current user';
+                    buttonPost.setAttribute('data-visible', 'false');
+                } else {
+                    showUserPosts(user.id);
+                    buttonPost.innerText = 'Hide Posts';
+                    buttonPost.setAttribute('data-visible', 'true');
+                }
             };
 
             div.appendChild(usersInfo);
             div.appendChild(buttonPost);
             container.appendChild(div);
         }
-    })
+    });
+
 
 function showUserPosts(userId) {
     fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
         .then(response => response.json())
         .then(posts => {
-            const postsContainer = document.getElementById('posts-container') || document.createElement('div');
-            postsContainer.id = 'posts-container';
-            postsContainer.innerHTML = '';
+            const postsContainer = document.createElement('div');
+            postsContainer.id = `posts-container-${userId}`;
             posts.forEach(post => {
-                const postElement = document.createElement('p');
-                postElement.innerText = `Title: ${post.title}`;
+                const postElement = document.createElement('div');
+                postElement.innerHTML = `
+                    <p>Title: ${post.title}</p>
+                    <a href="post-details.html?postId=${post.id}">View Post Details</a>
+                `;
                 postsContainer.appendChild(postElement);
             });
 
             const userDiv = document.querySelector(`#full-info .newDiv:nth-child(${userId})`);
             userDiv.appendChild(postsContainer);
         });
+}
+
+
+function hideUserPosts(userId) {
+    const postsContainer = document.getElementById(`posts-container-${userId}`);
+    if (postsContainer) {
+        postsContainer.remove();
+    }
 }
